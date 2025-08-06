b635a3ee6b11d6c61df92c223deb1b6ac3231bf6
Joiver González <gjoiver@gmail.com>
2025-08-05 22:21:24 -0500

    feat(app): initial configuration

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-[--color-bg] text-gray-900">
+    <main class="container mx-auto py-10 px-4">
+      <h1 class="text-3xl font-bold mb-8 text-[--color-primary]">Mis Tareas</h1>
+      <HomeView />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import HomeView from './views/HomeView.vue'
+</script>
